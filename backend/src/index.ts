2b20bd6666dabfa832148d3ecbdad760da7af5a7
Nicolas Mastromarino <nmastromarino@teco.com.ar>
2025-11-20 16:48:14 -0300

    dominio agregado

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,9 @@ app.use(express.json());
 const allowedOrigins = [
   env.frontendUrl,
   'http://localhost:3000',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  'https://elconsejodehombres.net',
+  'https://www.elconsejodehombres.net'
 ];
 
 app.use(
